c3a58e087fe5264f74bfe666e9f00c8936f0939e
Bobitza <vlad.gozman@yahoo.com>
2023-01-06 16:27:18 +0200

    Rearranging stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,18 @@
 module home.riderly {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
     opens home.riderly to javafx.fxml;
     exports home.riderly;
+    exports home.riderly.Controllers;
+    exports home.riderly.Controllers.Admin;
+    exports home.riderly.Controllers.Client;
+    exports home.riderly.Models;
+    exports home.riderly.Views;
 }
