857785b261f73c1d6f7b311d1ae9c63618a4ce67
Bobitza <vlad.gozman@yahoo.com>
2023-01-07 22:47:50 +0200

    Added db and removed account selector

--- src/main/java/home/riderly/Controllers/LoginController.java
+++ src/main/java/home/riderly/Controllers/LoginController.java
@@ -3,7 +3,6 @@ package home.riderly.Controllers;
 import home.riderly.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -12,35 +11,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
-
-    public Label selector_lbl;
-    public ChoiceBox acc_selector;
-    public Label user_lbl;
+    public Label userLbl;
     public TextField user_txt;
-    public Label pass_lbl;
-    public TextField pass_txt;
-    public Button login_btn;
-    public Label err_lbl;
+    public Label passLbl;
+    public TextField passTxt;
+    public Button loginBtn;
+    public Label errLbl;
 
-    public void initialize() {
-        acc_selector.getItems().addAll("Client", "Administrator");
-        acc_selector.setValue("Client");
-    }
-
-    public void login_btnAction() {
-        System.out.println("Login");
-    }
-
-    public void acc_selectorAction() {
-        System.out.println("Account selector");
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        login_btn.setOnAction(event -> onLogin());
+        loginBtn.setOnAction(event -> onLogin());
     }
     private void onLogin(){
-        Stage stage = (Stage) err_lbl.getScene().getWindow();
+        Stage stage = (Stage) errLbl.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
         Model.getInstance().getViewFactory().showClientWindow();
     }
